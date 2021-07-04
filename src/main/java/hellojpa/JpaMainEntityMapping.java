49963ad3016b57d23ddd619f1165b4f994d0559f
sungwonyun <sungwon.yun@nhnsoft.com>
2021-07-04 16:15:53 +0900

    Tutorial-03
    엔티티 매핑

---
+++ src/main/java/hellojpa/JpaMainEntityMapping.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMainEntityMapping {
+
+	// 객체와 테이블 매핑 : @Entity, @Table
+	// 필드와 컬럼 매핑 : @Column
+	// 기본 키 매핑 : @Id
+	// 연관관계 매핑 : @ManyToOne, @JoinColumn
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			Member member = new Member();
+			member.setId(1L);
+			member.setName("HelloWorld");
+
+			em.persist(member);
+
+			tx.commit();
+		} catch (Exception e){
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+}
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,12 +1,19 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-//@Table(name = "MEMBER") 관례에 따라 entity와 테이블 명이 같은 경우 기술하지 않아도 무방
+@Entity // @Entity가 붙은 클래스는 JPA가 관리하는 엔티티라고 한다
+//@Entity(name = "NAME") JPA에서 사용할 엔티티 이름을 지정한다(미지정시 클래스 이름을 그대로 사용 - 추천)
+//@Table(name = "MEMBER", catalog = "CATALOG", schema = "SCHEMA", uniqueConstraints = "INDEX") 관례에 따라 entity와 테이블 명이 같은 경우 기술하지 않아도 무방
 public class Member {
+
+	//기본 생성자 필수(파라미터가 없는 public 또는 protected 생성자)
+	//final 클래스, enum, interface, inner 클래스 사용 불가
+	//저장할 필드에 final 사용 불가
+
 	public Member() {
 	}
 
@@ -17,6 +24,8 @@ public class Member {
 
 	@Id
 	private Long id;
+	//DDL 생성 기능 - DDL 생성에만 영향을 주고 JPA 실행에 영향이 가지 않는다
+	//@Column(name = "USERNAME", unique = true, length = 10)
 	private String name;
 
 	public Long getId() {
